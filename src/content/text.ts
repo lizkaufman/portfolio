6bde7a61f18264e54125d359eb3ffba03a073d48
Liz Kaufman <lizkaufman92@gmail.com>
2024-04-25 14:55:08 +0100

    feat: :sparkles: sets up basic hero section design

---
+++ src/content/text.ts
@@ -0,0 +1,15 @@
+export const homepage = {
+  summary: [
+    "Lorem ipsum here's a whole sentence. Lorem ipsum here's a whole sentence. Lorem ipsum here's a whole sentence. Lorem ipsum here's a whole sentence. ",
+  ],
+  workExperience: [
+    "I grew up just outside of Atlanta in the US and graduated with a history degree and three quarters of an archaeology degree.",
+    "I moved to the UK and worked in operations and project management while trying to decide what I actually wanted to be when I grew up. I'd been wondering if I could get into code for a while, and at the end of 2019, I decided to give it a shot. I was a bootcamper on School of Code's third cohort from Jan-May 2020, where I became a full-stack developer and discovered that code is the closest thing to magic in real life.",
+    "I had the privilege of being a coach at the School of Code for the next three years after graduating the bootcamp, where I've been able to share that magic with each cohort of bootcampers since, progressing to a role as Head of Bootcamps and leading the coaching team. I came into tech seeking a career that was challenging and meaningful, and it was incredibly rewarding to help people change their careers and lives with technology while bolstering my technical and communication skills (not to mention my ability to code and problem solve under pressure!).",
+    "I now work as a front end developer at Games Workshop, where I get to combine two things I love doing: painting Warhammer and writing code.",
+  ],
+  outsideOfWork: [
+    "Outside of work, I dote on my cat, try not to kill my houseplants, and collect ridiculous glasses. I love posh tea, cheap chocolate, and old records from the 60s and 70s.",
+    "When I'm not coding, I unwind by reading, attempting to write novels, listening to podcasts, and painting Warhammer figures.",
+  ],
+};
