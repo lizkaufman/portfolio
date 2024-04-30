953ea5f517a80f1e3e680aea9cda165c5adc9d73
Liz Kaufman <lizkaufman92@gmail.com>
2024-04-30 14:23:47 +0100

    feat: :lipstick: hero section now mobile responsive

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -1,15 +1,13 @@
-"use client";
-
 import Image from "next/image";
 import { homepage } from "@/content/text";
-import { Email, GitHub, LinkedIn } from "@mui/icons-material";
+import { Email, GitHub, LinkedIn, EmojiEmotions } from "@mui/icons-material";
 import LinkButton from "@/components/atoms/LinkButton/LinkButton";
 
 export type HeroProps = {};
 
 export default function Hero({}: HeroProps) {
   return (
-    <section className="flex flex-col items-center md:flex-row md:min-h-screen justify-between p-6 md:p-16 md:grid md:grid-cols-3">
+    <section className="flex flex-col items-center md:flex-row md:min-h-screen justify-between p-8 md:p-16 md:grid md:grid-cols-3 mt-4 md:mt-0">
       <div className="max-w-96 max-h-96 p-4 bg-white rounded-full flex items-center justify-self-center justify-center border-solid border-4 border-codingbumblebee-yellow">
         <Image
           className="w-3/4 h-auto"
@@ -20,14 +18,15 @@ export default function Hero({}: HeroProps) {
         />
       </div>
 
-      <div className="col-start-2 col-span-2 flex flex-col justify-around space-y-4 pl-8 m-0 mt-0">
+      <div className="col-start-2 col-span-2 flex flex-col justify-around space-y-4 p-4 pt-8 md:pl-8 text-center md:text-left">
         <h1 className="text-3xl m-0">Hello!</h1>
         <h2 className="text-xl">I'm Liz Kaufman.</h2>
         <p className="">{homepage.summary}</p>
-        <div className="flex flex-row items-center space-x-4">
+        <div className="flex flex-row items-center space-x-4 justify-center md:justify-start">
           <LinkButton href="">
             {/* TODO: set up to link to below the fold */}
-            <span>Learn more</span>
+            <span className="hidden md:inline">Learn more</span>
+            <EmojiEmotions className="md:hidden" />
           </LinkButton>
           <LinkButton href="https://www.linkedin.com/in/liz-kaufman/">
             <LinkedIn className="fill-codingbumblebee-dark-grey" />
